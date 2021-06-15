3ccf549f96647632340ec2e17795650c423c8be6
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2021-06-15 16:47:02 +0200

    refactor: change in projects

--- src/fixtures/projects.js
+++ src/fixtures/projects.js
@@ -1,13 +1,13 @@
 export const projects = [
   {
-    title: 'Strawpoll',
+    title: 'Poll App',
     description:
       'Website allowing you to quickly create real-time polls. Single or multiple selection, public or unlisted.',
-    tech: 'React, React-Router, Styled-Components, Firebase',
+    tech: 'React, TailwindCss, MongoDB, Apollo, GraphQL',
     direction: 'row',
     imgUrl: '/chart.png',
-    github: 'https://github.com/bartoszlipinski/strawpoll',
-    live: 'https://strawpoll.blipinski.pl/'
+    github: 'https://github.com/bartoszlipinski/poll-app',
+    live: 'https://poll-app.blipinski.pl/',
   },
   {
     title: 'Twitter Clone',
@@ -16,6 +16,6 @@ export const projects = [
     direction: 'row-reverse',
     imgUrl: '/twitter.png',
     github: 'https://github.com/bartoszlipinski/twitter-clone',
-    live: 'https://twitter-clone.blipinski.pl/'
-  }
+    live: 'https://twitter-clone.blipinski.pl/',
+  },
 ];
